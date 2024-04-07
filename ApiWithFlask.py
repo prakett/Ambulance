f25b0142c5b5c94a36a577e17a42be5035b52c64
praket <praketraj@gmail.com>
2024-04-07 13:44:59 +0530

    hehe

---
+++ ApiWithFlask.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+database = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'zxcvbnm',
+    'database': 'ambulance'
+}
+
+def connect_mysql():
+    return mysql.connector.connect(**database)
+
+@app.route('/ambulances', methods=['GET'])
+def ambulance():
+    try:
+        connection = connect_mysql()
+        cursor = connection.cursor(dictionary=True)
+
+        cursor.execute('SELECT * FROM ambulances')
+        ambulances = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+
+        return jsonify(ambulances)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
